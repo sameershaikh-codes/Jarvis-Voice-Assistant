9d9815d13b2a100622c9b8bb19632762c15a26a0
sameershaikh-codes <sameershaikh49910@gmail.com>
2026-01-22 19:40:09 +0530

    Update music_Library.py

--- music_Library.py
+++ music_Library.py
@@ -4,5 +4,8 @@ music = {
         "die" : "https://www.youtube.com/shorts/M5HKdTi-BFg?feature=share",
 
         "superman " : "https://www.youtube.com/shorts/TptcW8umyzk?feature=share",
+        
+        "bloody mary" : "https://youtu.be/YUWJoiiardE?si=Fus0iWMjmOLc3J6y",
 
 }
+
